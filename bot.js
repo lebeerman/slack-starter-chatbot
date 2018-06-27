9a2f49b2240f78d6a966bc516ce183ad5379e93d
Dan Beerman <omnidbeerman@gmail.com>
2018-06-27 17:22:36 -0600

    :eyes: updates and oops

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@
 
 This is a sample Slack bot built with Botkit.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+var CronJob = require('cron').CronJob;
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
@@ -31,9 +32,70 @@ var bot_options = {
 bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
 
 // Create the Botkit controller, which controls all instances of the bot.
-var controller = Botkit.slackbot(bot_options);
+const controller = Botkit.slackbot(bot_options);
 
+// create the bot instance for direct messages on a timer
+const bot = controller
+  .spawn({
+    token: process.env.slackbot_token
+  })
+  .startRTM();
 controller.startTicking();
+
+new CronJob('0 */1 * * * *', () => {
+    bot.api.im.open({
+      user: 'UBEN7TR38'
+    }, (err, res) => {
+      if (err) {
+        bot.botkit.log('Failed to open IM with user', err)
+      }
+      console.log(res);
+      bot.startConversation({
+        user: 'UBEN7TR38',
+        channel: res.channel.id,
+        text: 'WOWZA... 1....2'
+      }, (err, convo) => {
+        convo.say({
+              "text": "What did you work on this ____ (AM/PM)?",
+              "attachments": [{
+              "text": "Choose the project you've allocated time to:",
+              "fallback": "Sorry, you don't have any Projects or you arent part of a Unit.",
+              "callback_id": "project_work",
+              "color": "#FF6D27",
+              "attachment_type": "default",
+              "actions": [{
+                  "name": "test-proj1",
+                  "text": "Project 1",
+                  "style": "good",
+                  "type": "button",
+                  "value": "project1"
+                },
+                {
+                  "name": "test-proj2",
+                  "text": "Project 2",
+                  "style": "warning",
+                  "type": "button",
+                  "value": "project2"
+                },
+                {
+                  "name": "test-proj3",
+                  "text": "Project 3",
+                  "style": "danger",
+                  "type": "button",
+                  "value": "project3",
+                  "confirm": {
+                    "title": "Are you sure?",
+                    "text": "Have you really been working on this project?!",
+                    "ok_text": "DEFINITELY!",
+                    "dismiss_text": "No way!"
+                  }
+                }
+              ]
+          }]
+        })
+      })
+    })
+}, null, true, 'Africa/Lagos')
 
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 var webserver = require(__dirname + '/components/express_webserver.js')(controller);
